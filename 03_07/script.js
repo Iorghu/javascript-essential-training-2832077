cc2f5b542342842b735a881c645b36378859d32b
Georg Riegelnegg <georg.rie@hotmail.com>
2023-06-10 17:12:52 +0200

    creating two objects

---
+++ 03_07/script.js
@@ -0,0 +1,30 @@
+const mug = {
+  name: "My Coffee Mug",
+  volume: 200,
+  color: "beige",
+  content: 70,
+  typeOfContent: "Coffee",
+  isEmpty: function () {
+    return this.content === 0;
+  },
+  status: function () {
+    // if (this.isEmpty) return "empty"; // method was not called, but function was always true.
+    if (this.isEmpty()) return "empty";
+    if (!this.isEmpty) console.log("!this.isEmpty is truthy");
+    if (this.isEmpty) console.log("this.isEmpty is truthy");
+    if (this.content == this.volume) return "full";
+    return "not empty";
+  },
+};
+console.log(mug);
+console.log("content of mug:", mug["content"]);
+console.log(`${mug.name} is ${mug.status()}`);
+
+const book = {
+  title: "How to Create Objects in JavaScript",
+  difficulty: "easy",
+  numberOfPages: 0o11, // 9
+  worthWile: true,
+};
+console.log(`my favorite book is: ${book.title}`);
+console.log(`my favorite book is not: ${book}`);
